e8a288673fc4701cd1841093f992e4bfda8b8114
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-10 18:01:31 -0800

    Create user model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+    validates :username, :password_digest, :session_token, presence: true
+    validates :username, :session_token, uniqueness: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    after_initalize :ensure_session_token
+    attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        return user if user && user.valid_password?(password)
+        nil
+    end
+
+    def valid_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+end
