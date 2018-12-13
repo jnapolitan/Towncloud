19c9e5dfbcdfb995bae8bd3f9913cacc4b675293
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-12 16:32:17 -0800

    Update session form style

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -69,9 +69,8 @@ export default class SessionForm extends React.Component {
     return (
         <Modal isOpen={this.state.modalIsOpen} onAfterOpen={this.afterOpenModal} onRequestClose={this.closeModal} style={customStyles} contentLabel="Session Form Modal">
           <div className="session-form-container">
-            {/* <div className="session-form-logo"></div> */}
             <h1>{this.props.formType}</h1>
-            <form onSubmit={this.handleSubmit}>
+              <form onSubmit={this.handleSubmit}>
               <input type="text" placeholder="Your username" value={this.state.username} onChange={this.update("username")} />
               <input type="password" placeholder="Your password" value={this.state.password} onChange={this.update("password")} />
               <ul className="session-form-errors">{errors}</ul>
@@ -87,7 +86,7 @@ export default class SessionForm extends React.Component {
               &nbsp;<a href="https://www.google.com/search?safe=off&tbm=isch&source=hp&biw=1760&bih=1066&ei=-FwRXJv2AoHH_wScjr6YBA&q=puppies&oq=puppies&gs_l=img.3..35i39j0l9.787.1439..1523...0.0..1.59.368.7......1....1..gws-wiz-img.....0.nYpm3VCEIkQ" target="_blank">
                   Privacy Policy</a>.
               </p>
-              <button className="demo-login-btn" onClick={() => this.props.demoLogin(demoUser)}>Demo</button>
+               <button className="demo-login-btn" onClick={() => this.props.demoLogin(demoUser)}>Demo</button>
             </form>
           </div>
         </Modal>
