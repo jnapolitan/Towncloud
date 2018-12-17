b864a41cebb5962cf9d2d43eebe569b2c568c114
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-17 14:21:31 -0800

    Seed fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.digest = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
