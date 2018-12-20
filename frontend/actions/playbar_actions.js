233f68eeb53d5d9b112843641cc9eb8ef7a52632
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-19 17:13:10 -0800

    Fix playbar position

--- frontend/actions/playbar_actions.js
+++ frontend/actions/playbar_actions.js
@@ -1,6 +1,7 @@
 export const RECEIVE_PLAYER_SONG = 'RECEIVE_PLAYER_SONG';
 export const TOGGLE_PLAY_PAUSE = 'TOGGLE_PLAY_PAUSE';
 export const RECEIVE_CURRENT_TIME = 'RECEIVE_CURRENT_TIME';
+export const RECEIVE_SEEK_TIME = 'RECEIVE_SEEK_TIME';
 
 export const receivePlayerSong = song => ({
     type: RECEIVE_PLAYER_SONG,
@@ -12,7 +13,7 @@ export const togglePlayPause = () => ({
 });
 
 export const receiveSeekTime = time => ({
-    type: SEEK_SONG,
+    type: RECEIVE_SEEK_TIME,
     time
 });
 
